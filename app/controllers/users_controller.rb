4fe312292cd2314998c0c8e5e6dce1c9a1b4aaa9
Mathilda Thompson <mathildathompson288@Mathildas-MacBook-Pro.local>
2013-06-05 16:16:02 +0100

    Refactored tool bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,5 @@ class UsersController < ApplicationController
     def index
      @currentuser = User.facebook(current_user.token)
      @image_url = current_user.image_url
-     binding.pry
  end
 end

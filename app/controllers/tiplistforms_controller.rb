23d5f7cc2560e66af923df84abac5845ee3ef344
Mathilda Thompson <mathildathompson288@Mathildas-MacBook-Pro.local>
2013-06-12 05:13:41 +0100

    I am going to commit this before I move things into a partial

--- app/controllers/tiplistforms_controller.rb
+++ app/controllers/tiplistforms_controller.rb
@@ -4,6 +4,12 @@ class TiplistformsController < ApplicationController
 
   def create
    binding.pry
+   advisable.class = params[:tiplist][:name]
+   advisable.class.save!
+   #render JS that knows how to append html to page
+  end
+
+  def new
   end
 
   def show

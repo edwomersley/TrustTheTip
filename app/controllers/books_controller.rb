82f213b95344d8f38e2ad925403c2987f1b14a11
Mathilda Thompson <mathildathompson288@Mathildas-MacBook-Pro.local>
2013-06-12 19:47:09 +0100

    Added CSS styling to the book show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,11 @@
 class BooksController < ApplicationController
     def index
+        # @userimage = current_user.image_url
+        @users = User.all 
+        
+        # @useradvisables = current_user.advisables
+        # @bookadvisables= @useradvisables.where(:type => "Book").first(10)
+        # binding.pry
     end
     
 end
